8a480c404c3e645d41655f0572f452a9492b5b32
mecy <1305996736@qq.com>
2021-05-16 11:36:29 +0800

    提交

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,13 +1,13 @@
 const TokenKey = 'Token';
 
 export function getToken() {
-  return window.localStorage.getItem(TokenKey);
+  return window.localStorage.getItem(TokenKey)
 }
 
 export function setToken(token) {
-  return window.localStorage.setItem(TokenKey, token);
+  return window.localStorage.setItem(TokenKey, token)
 }
 
 export function removeToken() {
-  return window.localStorage.removeItem(TokenKey);
+  return window.localStorage.removeItem(TokenKey)
 }
